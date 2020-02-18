c487f8d413b607eeeb58fbeea78896cc7fb2b225
Luis F. L. Muniz <luis.fernandolmuniz@gmail.com>
2020-02-17 22:32:40 -0300

    5° capitulo finalizado

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -6,17 +6,25 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputValor = $('#valor');
         this._inputQuantidade = $('#quantidade');
+        this._negociacoes = new Negociacoes();
     }
 
     adiciona(event) {
 
         event.preventDefault();
+        this._negociacoes.adiciona(this._criaNegociacao());        
+        this._limparFormulario();
+    }
 
+    _limparFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
+
+    _criaNegociacao(){
         let data = DateConverter.paraData(this._inputData.value);
-        let negociacao = new Negociacao(data, parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
-
-        console.log(negociacao.data);
-        let ddmmaa = DateConverter.paraTexto(negociacao.data);
-        console.log(ddmmaa);
+        return new Negociacao(data, parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
     }
 }
